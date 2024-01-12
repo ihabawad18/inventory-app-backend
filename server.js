7dc1581617c6c316983de8931b328c8170d8d102
Ihab Awad <ihabawad767@Gmail.com>
2024-01-12 17:07:27 +0200

    commit

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:3000","https://inventory-app-2-ppay.onrender.com"],
     credentials: true,
   })
 );
